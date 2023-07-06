33fdec8cc6876a1f076b018ae38cf7d8069b9ea7
unaivan <unaivan@gmail.com>
2023-07-06 14:26:25 +0700

    Initial Commit

---
+++ ciap/ciapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ciapApp.swift
+//  ciap
+//
+//  Created by una ivan on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ciapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
