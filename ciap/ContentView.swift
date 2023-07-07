54b812ba88bfb322a3203f5cf139b290eb6b8dbc
unaivan <unaivan@gmail.com>
2023-07-07 11:10:27 +0700

    update list

--- ciap/ContentView.swift
+++ ciap/ContentView.swift
@@ -1,74 +1,129 @@
 import SwiftUI
 
-struct aaaa: View {
+struct ContentList: View {
     @ObservedObject var dataViewModel = DataViewModel()
-
+    
+    
     var body: some View {
-        NavigationView {
-            List(dataViewModel.posts) { post in
-                if let person = dataViewModel.person(for: post) {
-                    VStack(alignment: .leading) {
+        List(dataViewModel.posts) { post in
+            NavigationLink(destination: PostDetail(post: post, person: dataViewModel.person(for: post))) {
+                VStack(alignment: .leading) {
+                    HStack {
                         HStack {
-                            HStack {
-                                URLImage(urlString: person.photouser)
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: 32, height: 32)
-                                    .cornerRadius(52)
+                            URLImage(urlString: dataViewModel.person(for: post)?.photouser ?? "")
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 32, height: 32)
+                                .cornerRadius(52)
 
-                                Text(person.name)
-                                    .font(.headline)
-                            }
-                            Text(post.date)
+                            Text(dataViewModel.person(for: post)?.name ?? "")
                                 .font(.headline)
                         }
-
-                        Text(post.ciap)
-                            .foregroundColor(.secondary)
+                        Text(post.date)
+                            .font(.headline)
                     }
-//                    .padding()
+                    Text(post.ciap)
+                        .foregroundColor(.secondary)
                 }
-            }.listStyle(.plain)
-            .navigationTitle("Posts")
+//                    .padding()
+            }
+            .padding(.bottom, 8)
+            .padding(.top, 8)
+            .listRowSeparator(.hidden)
+        }.listStyle(.inset)
+        .navigationTitle("Posts")
+        .frame(width: UIScreen.main.bounds.width)
+    }
+}
+
+struct Home : View {
+    @State private var readyToNavigate : Bool = false
+    @Environment(\.presentationMode) var presentationMode
+    @State private var isShowingPageSearchDetail = false
+    
+    var body : some View {
+        
+        NavigationView{
+            ContentList()
+                .navigationBarTitleDisplayMode(.inline)
+            
+                .toolbar {
+                    
+                    ToolbarItem(placement: .navigationBarLeading) {
+//                        NavigationLink(destination: SearchDetail()) {
+//                            Image(systemName: "magnifyingglass.circle.fill").foregroundColor(.gray)
+//                                    .font(.system(size: 24))
+//                        }.navigationBarTitle("Home", displayMode: .inline)
+//                            .navigationBarBackButtonHidden(true)
+                        
+                        Button(action: {
+                            isShowingPageSearchDetail = true
+                        }) {
+                            Image(systemName: "magnifyingglass.circle.fill").foregroundColor(.gray)
+                                    .font(.system(size: 24))
+                        }
+                        
+                    }
+                    
+                    ToolbarItem(placement: .principal) {
+                        VStack{
+                            Image("Ciapciap")
+                                .frame(width: 200, height: 30, alignment: .center)
+                        }
+                    }
+
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack(spacing: 2){
+                            Image("sort")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 18))
+                        }
+                    }
+            }.sheet(isPresented: $isShowingPageSearchDetail) {
+//                SearchDetail()
+            }
         }
     }
 }
 
-
 struct ContentView: View {
-    @ObservedObject var dataViewModel = DataViewModel()
-    
-    
+    @State private var selectedTab = 0
     var body: some View {
-        NavigationView {
-            List(dataViewModel.posts) { post in
-                NavigationLink(destination: PostDetail(post: post, person: dataViewModel.person(for: post))) {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            HStack {
-                                URLImage(urlString: dataViewModel.person(for: post)?.photouser ?? "")
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-                                    .cornerRadius(25)
-
-                                Text(dataViewModel.person(for: post)?.name ?? "")
-                                    .font(.headline)
-                            }
-                            Text(post.date)
-                                .font(.headline)
-                        }
-                        
-                        Text("Background: \(post.background)")
-                            .foregroundColor(.secondary)
-                        Text("Date: \(post.date)")
-                            .foregroundColor(.secondary)
-                        Text("CIAP: \(post.ciap)")
-                            .foregroundColor(.secondary)
-                    }
-//                    .padding()
-                }
-            }.listStyle(.plain)
-            .navigationTitle("Posts")
-        }
+        VStack{
+            TabView(selection: $selectedTab) {
+                Home()
+                    .tabItem {
+                        Image("homeIcon")
+                            .renderingMode(.template)
+                            .environment(\.symbolVariants, .none)
+                        Text("Home")
+                    }.tag(0)
+                
+                Home()
+                    .tabItem {
+                        Image("homeIcon")
+                            .renderingMode(.template)
+                            .environment(\.symbolVariants, .none)
+                        Text("History")
+                    }.tag(1)
+                
+                Home()
+                    .tabItem {
+                        Image("homeIcon")
+                            .renderingMode(.template)
+                            .environment(\.symbolVariants, .none)
+                        Text("Saved")
+                    }.tag(2)
+                
+                ProfileDetail()
+                    .tabItem {
+                        Image("profile")
+                            .renderingMode(.template)
+                            .environment(\.symbolVariants, .none)
+                        Text("Profile")
+                    }.tag(3)
+            }
+            .accentColor(.black)
+        }.background(Color.red)
     }
 }
 
